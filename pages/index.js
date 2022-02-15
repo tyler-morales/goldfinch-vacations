b70ecedb44cf5a92a197e4f93d36aec0fe739e89
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-15 14:13:18 -0600

    💄 Code newsletter UI

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Hero from '../components/Hero'
 import Inside from '../components/Inside'
+import Newsletter from '../components/Newsletter'
 
 export default function Home() {
   return (
     <main className="grid gap-20 mt-10 md:mt-20">
       <Hero />
       <Inside />
+      <Newsletter />
     </main>
   )
 }
