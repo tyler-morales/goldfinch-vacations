2a8d65a0ba52798babcc3592221813cad9d47cb9
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-14 19:30:47 -0600

    🚧  Get Swiper working Loaded images and made it responsive

--- components/Inside.js
+++ components/Inside.js
@@ -1,11 +1,25 @@
 import Layout from './Layout'
 import PhotoSwiper from './PhotoSwiper'
 
+const livingAreas = [
+  {url: '/../public/images/living areas/img1.jpg', alt: 'one'},
+  {url: '/../public/images/living areas/img2.jpg', alt: 'two'},
+  {url: '/../public/images/living areas/img3.jpg', alt: 'three'},
+  {url: '/../public/images/living areas/img4.jpg', alt: 'four'},
+  {url: '/../public/images/living areas/img5.jpg', alt: 'five'},
+  {url: '/../public/images/living areas/img6.jpg', alt: 'six'},
+  {url: '/../public/images/living areas/img7.jpg', alt: 'seven'},
+  {url: '/../public/images/living areas/img8.jpg', alt: 'eight'},
+]
+
 export default function Inside() {
   return (
-    <Layout>
-      <h2 className="text-3xl font-bold dark:text-white">Inside the house</h2>
-      <PhotoSwiper />
-    </Layout>
+    <>
+      <Layout>
+        <h2 className="text-3xl font-bold dark:text-white">Inside the house</h2>
+      </Layout>
+      <PhotoSwiper images={livingAreas} />
+      <Layout></Layout>
+    </>
   )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Layout({children}) {
-  return <div className="max-w-[1200px] m-auto w-full px-4">{children}</div>
+  return <div className="max-w-[1200px] m-auto px-4">{children}</div>
 }
